4fd0acab328aa6059158f22f21559f800183aa61
seva <seva@ada.support>
2021-02-04 15:27:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,106 @@
+# eslint-plugin-import-newlines
+
+ESLint plugin for enforcing newlines in ES6 import statements past a certain number of items.
+
+There is only one rule in this plugin which will report when there are more than 4 values in a line by default, and if there are less it will report when the import is not on a single line.
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-import-newlines`:
+
+```
+$ npm install eslint-plugin-import-newlines --save-dev
+```
+
+**Note:** If you installed ESLint globally (using the `-g` flag) then you must also install `eslint-plugin-import-newlines` globally.
+
+## Usage
+
+Add `import-newlines` to the plugins section of your `.eslintrc` configuration file.
+
+```json
+{
+    "plugins": [
+        "import-newlines"
+    ]
+}
+```
+
+Then add the rule in the rules section.
+
+```json
+{
+    "rules": {
+        "import-newlines/enforce": "error"
+    }
+}
+```
+
+### Options
+
+The first and most readable way is to use an object which allows you to specify any of the available options, leaving everything that's not specified as the default.
+
+  ```json
+  {
+      "rules": {
+          "max-len": ["error", 100],
+          "import-newlines/enforce": [
+              "error",
+              {
+                  "items": 2,
+                  "max-len": 100
+              }
+          ]
+      }
+  }
+  ```
+
+Because of its easy expandability this is the only format that will allow you to configure any new options &ndash; should they be added &ndash; but there is also a shorter syntax that will let you configure just the first two and keep everything else default.
+
+To specify `items` as **6**:
+
+  ```json
+  {
+      "rules": {
+          "import-newlines/enforce": [
+              "error",
+              6
+          ]
+      }
+  }
+  ```
+
+To specify `items` as **4** and `max-len` as **120**:
+
+  ```json
+  {
+      "rules": {
+          "max-len": ["error", 120],
+          "import-newlines/enforce": [
+              "error",
+              4,
+              120
+          ]
+      }
+  }
+  ```
+
+#### `items` [number] (default: `4`)
+
+Specifies the maximum number of items before the plugin requires breaking up the `import` to multiple lines. If there are exactly this many or fewer items, then the plugin will make sure the import stays on one line unless it would violate the `max-len` option. More items than this number will always be split onto multiple lines.
+
+Note that the plugin simply inserts newline characters after each token in the import when splitting, and the fix output never includes leading tabs or spaces. To have consistent indentation, be sure to use the built-in `indent` rule.
+
+#### `max-len` [number] (default: `Infinity`)
+
+Specifies the maximum length for source code lines in your project. This allows the plugin to prevent quick fixes that would cause your code to violate this limit from being applied. The rule will also automatically split import lines for you should they exceed the limit, which works great as an automatic fix for the ESLint built-in `max-len` rule (which doesn't have any quick fixes out of the box at the time of writing) for your imports. It's highly recommended you keep this option's value in sync with what you use for the aforementioned rule for best results.
+
+### Testing
+
+Tests can be run via `npm run test`, make sure these pass after every change. Be sure to add tests for new features.
---
+++ test/enforce.test.js
@@ -0,0 +1,183 @@
+/* eslint-env mocha */
+const { RuleTester } = require("eslint");
+const rule = require("../lib/enforce.js");
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 2015,
+    sourceType: "module",
+  },
+});
+
+const repeatString = (str, times) => [...new Array(times + 1)].join(str);
+
+ruleTester.run("enforce", rule, {
+  valid: [
+    {
+      code: "const { a, b, c, d } = test",
+    },
+    {
+      code: "const { a, b, c, d } = test",
+      options: [6],
+    },
+    {
+      code: "const { a, b, c, d } = test",
+      options: [{
+        items: 6,
+      }],
+    },
+    {
+      code: "const {\na,\nb,\nc\n} = test",
+      options: [2],
+    },
+    {
+      code: "const {\na,\nb,\nc\n} = test",
+      options: [{
+        items: 2,
+      }],
+    },
+    {
+      code: "const { a, b, c, d } = test",
+      options: [4, 50],
+    },
+    {
+      code: "const { a, b, c, d } = test",
+      options: [{
+        items: 4,
+        "max-len": 50,
+      }],
+    },
+    {
+      code: `const { ${repeatString("a", 25)} } = test`,
+      options: [1, 50],
+    },
+    {
+      code: `const { ${repeatString("a", 25)} } = test`,
+      options: [{
+        items: 1,
+        "max-len": 50,
+      }],
+    },
+    {
+      code: `const {\n${repeatString("a", 25)},\n${repeatString("b", 25)}\n} = test`,
+      options: [6, 50],
+    },
+    {
+      code: `const {\n${repeatString("a", 25)},\n${repeatString("b", 25)}\n} = test`,
+      options: [{
+        items: 6,
+        "max-len": 50,
+      }],
+    },
+    {
+      code: "const { a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t } = test",
+      options: [20, 83],
+    },
+    {
+      code: "const { a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t } = test",
+      options: [{
+        items: 20,
+        "max-len": 83,
+      }],
+    },
+    {
+      code: `const { ${repeatString("a", 512)} } = test`,
+      options: [1],
+    },
+    {
+      code: `const { ${repeatString("a", 512)} } = test`,
+      options: [{
+        items: 1,
+      }],
+    },
+  ],
+
+  invalid: [
+    {
+      code: "const {\na,\nb\n} = test",
+      output: "const { a, b } = test",
+      options: [4],
+      errors: [{ messageId: "mustNotSplit" }],
+    },
+    {
+      code: "let { a, b, c, d } = test",
+      output: "let {\n  a,\n  b,\n  c,\n  d,\n} = test",
+      options: [1],
+      errors: [{ messageId: "mustSplitMany" }],
+    },
+    {
+      code: "const { a, b, c, d } = object;",
+      output: "const {\n  a,\n  b,\n  c,\n  d,\n} = object;",
+      options: [{
+        items: 1,
+      }],
+      errors: [{ messageId: "mustSplitMany" }],
+    },
+    {
+      code: "const { \na,\n\nb,\n\n\nc,\n\n\nd,\n\n\n\ne\n} = test",
+      output: "const {\n  a,\n  b,\n  c,\n  d,\n  e,\n} = test",
+      options: [4],
+      errors: [{ messageId: "noBlankBetween" }],
+    },
+    {
+      code: "const { a,\n\n\nb } = test",
+      output: "const {\n  a,\n  b,\n} = test",
+      options: [1],
+      errors: [{ messageId: "noBlankBetween" }],
+    },
+    {
+      code: "const {\n\na,\nb\n\n\n} = test",
+      output: "const {\n  a,\n  b,\n} = test",
+      options: [1],
+      errors: [{ messageId: "limitLineCount" }],
+    },
+    {
+      code: "const {\na, b, c\n} = test",
+      output: "const {\n  a,\n  b,\n  c,\n} = test",
+      options: [1],
+      errors: [{ messageId: "limitLineCount" }],
+    },
+    {
+      code: "const { getPublicStaticVoidFinalObjectClassExtensionFactory } = test",
+      output: "const {\n  getPublicStaticVoidFinalObjectClassExtensionFactory,\n} = test",
+      options: [4, 50],
+      errors: [{ messageId: "mustSplitLong" }],
+    },
+    {
+      code: "const { aaaaaaaaaa, bbbbbbbbbb, cccccccccc, dddddddddd } = test",
+      output: "const {\n  aaaaaaaaaa,\n  bbbbbbbbbb,\n  cccccccccc,\n  dddddddddd,\n} = test",
+      options: [4, 50],
+      errors: [{ messageId: "mustSplitLong" }],
+    },
+    {
+      code: "const {\n    aaaaaaaaaaa,\n    aaaaaaaaaaaaaaaaaaa,\n    aaaaaaaaaaaaa\n} = aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+      output: "const { aaaaaaaaaaa, aaaaaaaaaaaaaaaaaaa, aaaaaaaaaaaaa } = aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+      options: [{
+        items: 4,
+        "max-len": 140,
+        semi: false,
+      }],
+      errors: [{ messageId: "mustNotSplit" }],
+    },
+    {
+      code: "const { aaaaaaaaaaa, aaaaaaaaaaaaaaaaaaa, aaaaaaaaaaaaa } = aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;",
+      output: "const {\n  aaaaaaaaaaa,\n  aaaaaaaaaaaaaaaaaaa,\n  aaaaaaaaaaaaa,\n} = aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;",
+      options: [{
+        items: 4,
+        "max-len": 140,
+        semi: true,
+      }],
+      errors: [{ messageId: "mustSplitLong" }],
+    },
+    {
+      code: "const { aaaaaaaaaaa: b, aaaaaaaaaaaaaaaaaaa, aaaaaaaaaaaaa } = aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;",
+      output: "const {\n  aaaaaaaaaaa: b,\n  aaaaaaaaaaaaaaaaaaa,\n  aaaaaaaaaaaaa,\n} = aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;",
+      options: [{
+        items: 4,
+        "max-len": 140,
+        semi: true,
+      }],
+      errors: [{ messageId: "mustSplitLong" }],
+    },
+  ],
+});
